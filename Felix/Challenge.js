3ffe99ee667305de39c155de2060d510ca318cc0
FelixB02 <felix.blechl@chello.at>
2023-02-07 18:18:11 +0100

    Challenge

--- Felix/Challenge.js
+++ Felix/Challenge.js
@@ -2,8 +2,10 @@ document.getElementById("btn").disabled = false;
 let fname = document.getElementById("player1");
 let lname = document.getElementById("player2");
 
+
 let number1 = 0;
 let number2 = 0;
+
 
 function getValues() {
     fname_value = fname.value;
@@ -15,7 +17,6 @@ function getValues() {
     document.getElementById("btn").disabled = true;
 }
 
-document.getElementById("btn").addEventListener("click", getValues);
 
 function randomNumber1() {
     number1 = Math.floor(Math.random() * 6) + 1;
@@ -26,8 +27,10 @@ function randomNumber1() {
 
 }
 
+
 function randomNumber2() {
     document.getElementById("score2").disabled = true;
+    document.getElementById("reset").disabled = false;
 
     number2 = Math.floor(Math.random() * 6) + 1;
     document.getElementById("dice_2").innerHTML = `${lname.value} got the value ${number2}`
@@ -41,7 +44,23 @@ function randomNumber2() {
 }
 
 
+function playAgain() {
+    document.getElementById("dice_1").innerHTML = "";
+    document.getElementById("dice_2").innerHTML = "";
+    number1 = 0;
+    number2 = 0;
+    document.getElementById("score1").disabled = false;
+    document.getElementById("reset").disabled = true;
+
+}
+
+function reloadsite() {
+    location.reload();
+}
 
 
 document.getElementById("score1").addEventListener("click", randomNumber1)
 document.getElementById("score2").addEventListener("click", randomNumber2)
+document.getElementById("reset").addEventListener("click", playAgain)
+document.getElementById("btn").addEventListener("click", getValues);
+document.getElementById("sitereload").addEventListener("click", reloadsite)
