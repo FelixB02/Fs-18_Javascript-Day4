002a6d0db8d7a8550b6853d57f3908213f9a9906
FelixB02 <felix.blechl@chello.at>
2023-02-07 22:58:02 +0100

    Adding Comments

--- Felix/Blackjack.js
+++ Felix/Blackjack.js
@@ -1,12 +1,15 @@
+// Declaring starting Variables
+
 document.getElementById("btn").disabled = false;
 let fname = document.getElementById("player1");
 let lname = document.getElementById("player2");
 let number1 = 0;
 let number2 = 0;
 
+// Calling the dealers starting function
 deal2cards();
 
-
+// get the Value out of the input field
 function getValues() {
     lname_value = lname.value;
     document.getElementById("name2").innerHTML = lname_value;
@@ -16,10 +19,11 @@ function getValues() {
     document.getElementById("btn").disabled = true;
 }
 
-
+// Dealing 2 starting Cards to the dealer
 function deal2cards() {
     number1 += Math.floor(Math.random() * 19) + 3;
     document.getElementById("dice_1").innerHTML = `Dealer: ${number1}`
+        // Checking if the dealer has a blackjack
     if (number1 == 21) {
         document.getElementById("winner").innerHTML = `The dealer has a blackjack!`
         document.getElementById("reset").disabled = false;
@@ -28,16 +32,18 @@ function deal2cards() {
     }
 }
 
-
+// Dealing the Cards to the Player
 function randomNumber2() {
     number2 += Math.floor(Math.random() * 11) + 2;
     document.getElementById("dice_2").innerHTML = `${lname.value}: ${number2}`
+        // Checking if the player has a blackjack
     if (number2 == 21) {
         document.getElementById("winner").innerHTML = `${lname_value} has a blackjack!`
         document.getElementById("reset").disabled = false;
         document.getElementById("score2").disabled = true;
         document.getElementById("stop").disabled = true;
     }
+    // Checking if the score is bigger than the 21 max score
     if (number2 > 21) {
         document.getElementById("score2").disabled = true;
         document.getElementById("winner").innerHTML = `${lname_value} busts!`
@@ -46,7 +52,7 @@ function randomNumber2() {
 
 }
 
-
+// Dealing the other cards to dealer after player stays
 function stop() {
     document.getElementById("reset").disabled = false;
     document.getElementById("score2").disabled = true;
@@ -54,6 +60,7 @@ function stop() {
         number1 += Math.floor(Math.random() * 11) + 1;
         document.getElementById("dice_1").innerHTML = `Dealer: ${number1}`
     }
+    // Getting the results and comparing them to declare the winner
     if (number1 > 21) {
         document.getElementById("winner").innerHTML = `The dealer busts`
     } else {
@@ -71,7 +78,7 @@ function stop() {
     }
 }
 
-
+// Playing again
 function playAgain() {
     document.getElementById("dice_1").innerHTML = "";
     document.getElementById("dice_2").innerHTML = "";
@@ -86,13 +93,13 @@ function playAgain() {
 
 }
 
-
+// Reloading site to clear all the variables
 function reloadsite() {
     location.reload();
 }
 
 
-
+// Buttons which are calling the different functions
 document.getElementById("score2").addEventListener("click", randomNumber2)
 document.getElementById("reset").addEventListener("click", playAgain)
 document.getElementById("btn").addEventListener("click", getValues);
